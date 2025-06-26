903857800a783f5ecaa37bdad52b032143e70564
Juan Jaramillo <info@juanjaramillo.tech>
2025-06-26 18:45:03 -0500

    refactor: Update credit card recommender page
    This commit refactors the credit card recommender page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,9 @@ async function getHighlighter() {
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+  site: config.site.base_url
+    ? config.site.base_url
+    : "https://budgetbeepro.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   vite: { plugins: [tailwindcss()] },
